61a350fe097c12f678711064af4bd76403243652
Aayush829 <73593921+Aayush829@users.noreply.github.com>
2023-11-22 19:32:10 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose")
+
+mongoose.connect("mongodb+srv://shivansh:Aayush829@cluster0.vgg2wap.mongodb.net/pinterest")
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }],
+  dp: {
+    type: String, // Assuming a URL or file path for the profile picture
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.plugin(plm);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
