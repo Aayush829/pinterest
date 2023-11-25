0efae9af2e35ff0a4467133563ddb4931c1f0cc5
Aayush829 <73593921+Aayush829@users.noreply.github.com>
2023-11-25 10:12:45 +0530

    adding multer

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const postModel = require('./posts')
 const localStrategy = require("passport-local");
 const passport = require('passport')
 passport.use(new localStrategy(userModel.authenticate()));
+const upload = require('./multer')
 
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -17,10 +18,27 @@ router.get('/login', function(req,res){
 router.get('/feed', function(req,res){
   res.render('feed');
 })
+
+router.post('/upload', upload.single("file"), async function(req,res,next){
+  if(!req.file){
+    return res.status(404).send("no files were given")
+  }
+  const user = await userModel.findOne({username: req.session.passport.user });
+  const post = await postModel.create({
+      image: req.file.filename,
+      imageText: req.body.filecaption,
+      user: user._id
+    })
+    user.posts.push(post._id);
+    await user.save()
+    res.redirect("/profile");
+});
+
 router.get('/profile',isLoggedIn, async function(req,res,next){
   const user = await userModel.findOne({
     username: req.session.passport.user
   })
+  .populate("posts")
   res.render("profile", {user});
 });
 
